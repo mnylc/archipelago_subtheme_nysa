dfc4d35e52aca2ab6e30963435b42193f6c97f90
Diego Pino Navarro <DiegoPino@users.noreply.github.com>
2024-09-06 10:44:08 -0400

    add scrolled observer

--- build/js/main.script.js
+++ build/js/main.script.js
@@ -3133,6 +3133,31 @@ __webpack_require__.r(__webpack_exports__);
 
 
 // * Any other global site-wide JavaScript should be placed below.
+(function ($, Drupal) {
+  'use strict';
+
+  Drupal.behaviors.bootstrap_nysa = {
+    attach: function attach(context, settings) {
+      var position = $(window).scrollTop();
+      $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+          $('body').addClass("scrolled");
+        } else {
+          $('body').removeClass("scrolled");
+        }
+        var scroll = $(window).scrollTop();
+        if (scroll > position) {
+          $('body').addClass("scrolldown");
+          $('body').removeClass("scrollup");
+        } else {
+          $('body').addClass("scrollup");
+          $('body').removeClass("scrolldown");
+        }
+        position = scroll;
+      });
+    }
+  };
+})(jQuery, Drupal);
 
 /***/ }),
 
--- src/js/main.script.js
+++ src/js/main.script.js
@@ -2,3 +2,31 @@
 import "./_bootstrap";
 
 // * Any other global site-wide JavaScript should be placed below.
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.bootstrap_nysa = {
+    attach: function (context, settings) {
+
+      var position = $(window).scrollTop();
+      $(window).scroll(function () {
+        if ($(this).scrollTop() > 50) {
+          $('body').addClass("scrolled");
+        }
+        else {
+          $('body').removeClass("scrolled");
+        }
+        var scroll = $(window).scrollTop();
+        if (scroll > position) {
+          $('body').addClass("scrolldown");
+          $('body').removeClass("scrollup");
+        } else {
+          $('body').addClass("scrollup");
+          $('body').removeClass("scrolldown");
+        }
+        position = scroll;
+      });
+    }
+  };
+})(jQuery, Drupal);
